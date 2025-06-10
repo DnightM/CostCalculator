6d99c0a653656004d9c54421432fd465a45f3ebd
JiHoon <kjhktc@naver.com>
2025-06-11 02:29:48 +0900

    변경중

--- js/util/ItemData.js
+++ js/util/ItemData.js
@@ -1,158 +1,158 @@
-class ItemData {
-    data;
-    /*
-        {
-            1:node,
-            2:node
-            ...
-        }
-
-        node
-        {
-            id:1,
-            name:"sample",
-            cost:100, // 원가
-            salesPrice:100, // 판매비용
-            quantity:10, // 수량(용량)
-            type:"g" // g=group, i=item, m=material
-            children:{}, // children {2: {id:2, quantity:10}, ... }
-        }
-     */
-
-    idManager = new IdManager();
-    urlDataManager = new UrlDataManger();
-
-    _TYPE_GROUP = 'g';
-    _TYPE_ITEM = 'i';
-    _TYPE_MATERIAL = 'm';
-
-    constructor(data = {}) {
-        this.data = data;
-        for (const id of Object.keys(data)) {
-            this.idManager.add(id);
-        }
-
-        const methodNames = Object.getOwnPropertyNames(Object.getPrototypeOf(this));
-
-        methodNames.forEach(methodName => {
-            if (methodName.startsWith('create')
-                || methodName.startsWith('delete')
-                || methodName.startsWith('modify')
-                || methodName === 'add'
-                || methodName === 'remove'
-            ) {
-                const originalMethod = this[methodName];
-
-                // 메서드 래핑
-                this[methodName] = function (...args) {
-                    console.time(`${methodName} Time`);
-                    const result = originalMethod.apply(this, args);
-                    console.timeEnd(`${methodName} Time`);
-
-                    console.time(`url save Time`);
-                    this.urlDataManager.saveToUrl(this.data);
-                    console.timeEnd(`url save Time`);
-
-                    return result;
-                }.bind(this);
-
-            }
-        });
-    }
-
-    createGroup(groupName) {
-        const groupId = this.idManager.createId();
-        this.data[groupId] = {
-            id: groupId,
-            name: groupName,
-            type: this._TYPE_GROUP,
-            children: {}
-        };
-        return groupId;
-    }
-
-    deleteGroup(groupId) {
-        // 하위 매뉴도 다 지워짐
-        delete this.data[groupId]
-    }
-
-    modifyGroup(groupId, groupName) {
-        this.data[groupId].name = groupName;
-    }
-
-    createItem(itemName, salesPrice, quantity = 1) {
-        const itemId = this.idManager.createId();
-        this.data[itemId] = {
-            id: itemId,
-            name: itemName,
-            salesPrice: salesPrice,
-            quantity: quantity,
-            type: this._TYPE_ITEM,
-            children: {}
-        };
-        return itemId;
-    }
-
-    deleteItem(itemId) {
-        delete this.data[itemId];
-
-        for (const id in this.data) {
-            delete this.data[id].children?.[itemId];
-        }
-    }
-
-    modifyItem(itemId, {name, salesPrice, quantity}) {
-        const item = this.data[itemId];
-        if (!item || item.type !== this._TYPE_ITEM) return;
-        if (name !== undefined) item.name = name;
-        if (salesPrice !== undefined) item.salesPrice = salesPrice;
-        if (quantity !== undefined) item.quantity = quantity;
-    }
-
-    createMaterial(name, cost, quantity = 1) {
-        const materialId = this.idManager.createId();
-        this.data[materialId] = {
-            id: materialId,
-            name: name,
-            cost: cost,
-            quantity: quantity,
-            type: this._TYPE_MATERIAL,
-            children: {}
-        };
-        return materialId;
-    }
-
-    deleteMaterial(materialId) {
-        delete this.data[materialId];
-
-        // 연결된 부모 노드에서도 제거
-        for (const id in this.data) {
-            delete this.data[id].children?.[materialId];
-        }
-    }
-
-    modifyMaterial(materialId, {name, cost, quantity}) {
-        const mat = this.data[materialId];
-        if (!mat || mat.type !== this._TYPE_MATERIAL) return;
-        if (name !== undefined) mat.name = name;
-        if (cost !== undefined) mat.cost = cost;
-        if (quantity !== undefined) mat.quantity = quantity;
-    }
-
-    add(parentId, childId, quantity) {
-        if (quantity) {
-            this.data[parentId].children[childId] = {
-                id: childId,
-                quantity: quantity
-            }
-        } else {
-            this.data[parentId].children[childId] = {
-                id: childId,
-            }
-        }
-    }
-
-    remove(parentId, childId) {
-        delete this.data[parentId].children[childId];
-    }
-}
+// class ItemData {
+//     data;
+//     /*
+//         {
+//             1:node,
+//             2:node
+//             ...
+//         }
+//
+//         node
+//         {
+//             id:1,
+//             name:"sample",
+//             cost:100, // 원가
+//             salesPrice:100, // 판매비용
+//             quantity:10, // 수량(용량)
+//             type:"g" // g=group, i=item, m=material
+//             children:{}, // children {2: {id:2, quantity:10}, ... }
+//         }
+//      */
+//
+//     idManager = new IdManager();
+//     urlDataManager = new UrlDataManger();
+//
+//     _TYPE_GROUP = 'g';
+//     _TYPE_ITEM = 'i';
+//     _TYPE_MATERIAL = 'm';
+//
+//     constructor(data = {}) {
+//         this.data = data;
+//         for (const id of Object.keys(data)) {
+//             this.idManager.add(id);
+//         }
+//
+//         const methodNames = Object.getOwnPropertyNames(Object.getPrototypeOf(this));
+//
+//         methodNames.forEach(methodName => {
+//             if (methodName.startsWith('create')
+//                 || methodName.startsWith('delete')
+//                 || methodName.startsWith('modify')
+//                 || methodName === 'add'
+//                 || methodName === 'remove'
+//             ) {
+//                 const originalMethod = this[methodName];
+//
+//                 // 메서드 래핑
+//                 this[methodName] = function (...args) {
+//                     console.time(`${methodName} Time`);
+//                     const result = originalMethod.apply(this, args);
+//                     console.timeEnd(`${methodName} Time`);
+//
+//                     console.time(`url save Time`);
+//                     this.urlDataManager.saveToUrl(this.data);
+//                     console.timeEnd(`url save Time`);
+//
+//                     return result;
+//                 }.bind(this);
+//
+//             }
+//         });
+//     }
+//
+//     createGroup(groupName) {
+//         const groupId = this.idManager.createId();
+//         this.data[groupId] = {
+//             id: groupId,
+//             name: groupName,
+//             type: this._TYPE_GROUP,
+//             children: {}
+//         };
+//         return groupId;
+//     }
+//
+//     deleteGroup(groupId) {
+//         // 하위 매뉴도 다 지워짐
+//         delete this.data[groupId]
+//     }
+//
+//     modifyGroup(groupId, groupName) {
+//         this.data[groupId].name = groupName;
+//     }
+//
+//     createItem(itemName, salesPrice, quantity = 1) {
+//         const itemId = this.idManager.createId();
+//         this.data[itemId] = {
+//             id: itemId,
+//             name: itemName,
+//             salesPrice: salesPrice,
+//             quantity: quantity,
+//             type: this._TYPE_ITEM,
+//             children: {}
+//         };
+//         return itemId;
+//     }
+//
+//     deleteItem(itemId) {
+//         delete this.data[itemId];
+//
+//         for (const id in this.data) {
+//             delete this.data[id].children?.[itemId];
+//         }
+//     }
+//
+//     modifyItem(itemId, {name, salesPrice, quantity}) {
+//         const item = this.data[itemId];
+//         if (!item || item.type !== this._TYPE_ITEM) return;
+//         if (name !== undefined) item.name = name;
+//         if (salesPrice !== undefined) item.salesPrice = salesPrice;
+//         if (quantity !== undefined) item.quantity = quantity;
+//     }
+//
+//     createMaterial(name, cost, quantity = 1) {
+//         const materialId = this.idManager.createId();
+//         this.data[materialId] = {
+//             id: materialId,
+//             name: name,
+//             cost: cost,
+//             quantity: quantity,
+//             type: this._TYPE_MATERIAL,
+//             children: {}
+//         };
+//         return materialId;
+//     }
+//
+//     deleteMaterial(materialId) {
+//         delete this.data[materialId];
+//
+//         // 연결된 부모 노드에서도 제거
+//         for (const id in this.data) {
+//             delete this.data[id].children?.[materialId];
+//         }
+//     }
+//
+//     modifyMaterial(materialId, {name, cost, quantity}) {
+//         const mat = this.data[materialId];
+//         if (!mat || mat.type !== this._TYPE_MATERIAL) return;
+//         if (name !== undefined) mat.name = name;
+//         if (cost !== undefined) mat.cost = cost;
+//         if (quantity !== undefined) mat.quantity = quantity;
+//     }
+//
+//     add(parentId, childId, quantity) {
+//         if (quantity) {
+//             this.data[parentId].children[childId] = {
+//                 id: childId,
+//                 quantity: quantity
+//             }
+//         } else {
+//             this.data[parentId].children[childId] = {
+//                 id: childId,
+//             }
+//         }
+//     }
+//
+//     remove(parentId, childId) {
+//         delete this.data[parentId].children[childId];
+//     }
+// }
--- js/util/TableController.js
+++ js/util/TableController.js
@@ -3,6 +3,7 @@ class TableController {
         this.dataTree = dataTree;
         this.tableElementSelector = tableElementSelector;
         this.table = null;
+        this.urlDataManager = new UrlDataManager();
     }
 
     show(id = null) {
@@ -28,7 +29,6 @@ class TableController {
                 const value = cell.getValue();
                 const node = this.dataTree.getNodeById(rowData.id);
                 if (!node) return;
-
                 try {
                     if (field === 'name') node.name = value;
                     else if (field === 'salesPrice') node.salesPrice = value;
@@ -59,7 +59,7 @@ class TableController {
                 data.push({
                     id: Number(childId),
                     name: node.name,
-                    quantity,
+                    quantity: quantity,
                     unitCost: node.unitCost,
                 });
             }
@@ -72,6 +72,7 @@ class TableController {
                         name: node.name,
                         salesPrice: node.salesPrice,
                         unitCost: node.unitCost,
+                        benefit: node.salesPrice - node.unitCost,
                     });
                 }
             }
@@ -83,16 +84,57 @@ class TableController {
     getColumns(id = null) {
         return id
             ? [
-                {title: "ID", field: "id"},
-                {title: "이름", field: "name", editor: "input"},
+                {title: "재료", field: "name", editor: "input"},
                 {title: "수량", field: "quantity", editor: "number"},
                 {title: "원가", field: "unitCost", editor: "number"}
             ]
             : [
-                {title: "ID", field: "id"},
-                {title: "이름", field: "name", editor: "input"},
+                {title: "메뉴", field: "name", editor: "input"},
                 {title: "판매가", field: "salesPrice", editor: "number"},
-                {title: "원가", field: "unitCost", editor: "number"}
+                {title: "원가", field: "unitCost"},
+                {title: "수익", field: "benefit"}
             ];
+    }
+
+    /**
+     * 테이블에 새 행 추가
+     * 현재 parentId 기준으로 재료 추가하거나, 최상단 메뉴 추가
+     */
+    insertRow(defaults = {}) {
+        const newId = this.dataTree.getNextId();
+        let newNode;
+
+        if (this.currentParentId) {
+            // 재료 추가
+            newNode = this.dataTree.createMaterial(defaults.name || "신규재료", defaults.unitCost || 0);
+            this.dataTree.addChild(this.currentParentId, newNode.id, defaults.quantity || 1);
+        } else {
+            // 메뉴 추가
+            newNode = this.dataTree.createItem(defaults.name || "신규메뉴", defaults.salesPrice || 0);
+        }
+
+        this.show(this.currentParentId);
+    }
+
+    /**
+     * 선택된 행 삭제
+     * DataTree에서도 제거
+     */
+    deleteRow() {
+        const selectedRows = this.table.getSelectedData();
+        if (selectedRows.length === 0) return;
+
+        for (const row of selectedRows) {
+            const id = row.id;
+            if (this.currentParentId) {
+                // 자식에서 제거
+                this.dataTree.removeChild(this.currentParentId, id);
+            } else {
+                // 전체에서 제거
+                this.dataTree.removeNode(id);
+            }
+        }
+
+        this.show(this.currentParentId);
     }
 }
